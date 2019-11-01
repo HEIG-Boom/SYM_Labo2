b99c5f42f425e281526381d99679ad3aed381071
Jael24 <jael.dubey@heig-vd.ch>
2019-11-01 14:17:41 +0100

    Launching various activities works

--- app/src/main/java/ch/heigvd/sym/labo_02/AsynchronousActivity.java
+++ app/src/main/java/ch/heigvd/sym/labo_02/AsynchronousActivity.java
@@ -1,4 +1,16 @@
 package ch.heigvd.sym.labo_02;
 
-public class AsynchronousActivity {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.Menu;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AsynchronousActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
--- app/src/main/java/ch/heigvd/sym/labo_02/CompressedActivity.java
+++ app/src/main/java/ch/heigvd/sym/labo_02/CompressedActivity.java
@@ -1,4 +1,15 @@
 package ch.heigvd.sym.labo_02;
 
-public class CompressedActivity {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CompressedActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
