79b8bbb51c9c10afc21052030fbc0344ceb5bfcd
Loris <loris.gilliand@heig-vd.ch>
2019-11-13 09:51:46 +0100

    symCom works

--- src/app/src/main/java/ch/heigvd/sym/labo2/comm/SymComManager.java
+++ src/app/src/main/java/ch/heigvd/sym/labo2/comm/SymComManager.java
@@ -2,6 +2,7 @@ package ch.heigvd.sym.labo2.comm;
 
 import android.os.AsyncTask;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -10,6 +11,8 @@ import java.net.URL;
 
 public class SymComManager extends AsyncTask<Request, Void, String> {
     private static final String TAG = SymComManager.class.getSimpleName();
+
+    private String response = "";
 
     private CommunicationEventListener communicationEventListener = null;
 
@@ -27,18 +30,43 @@ public class SymComManager extends AsyncTask<Request, Void, String> {
             URL url = new URL(requests[0].getUrl());
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setDoOutput(true);
+            con.setDoInput(true);
+            con.setRequestMethod("POST");
             con.setChunkedStreamingMode(0);
             con.setRequestProperty("Content-Type", requests[0].getContentType());
 
             BufferedOutputStream writer = new BufferedOutputStream(con.getOutputStream());
             writer.write(requests[0].getData().getBytes());
             writer.flush();
+
+            if (con.getResponseCode() == 200) {
+                BufferedInputStream reader = new BufferedInputStream(con.getInputStream());
+
+                byte[] contents = new byte[1024];
+
+                int bytesRead = 0;
+                while((bytesRead = reader.read(contents)) != -1) {
+                    response += new String(contents, 0, bytesRead);
+                }
+
+            }
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return "test";
+        return response;
+    }
+
+    protected void onPostExecute(String args) {
+        super.onPostExecute(args);
+
+        try {
+            communicationEventListener.handleServerResponse(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
