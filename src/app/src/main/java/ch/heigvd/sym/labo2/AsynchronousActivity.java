5af5eb9b576d9f676b0746acfa47e254838da920
Loris <loris.gilliand@heig-vd.ch>
2019-11-13 10:03:53 +0100

    eh voila

--- src/app/src/main/java/ch/heigvd/sym/labo2/AsynchronousActivity.java
+++ src/app/src/main/java/ch/heigvd/sym/labo2/AsynchronousActivity.java
@@ -3,27 +3,36 @@ package ch.heigvd.sym.labo2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import ch.heigvd.sym.labo2.comm.CommunicationEventListener;
 import ch.heigvd.sym.labo2.comm.Request;
 import ch.heigvd.sym.labo2.comm.SymComManager;
 
 public class AsynchronousActivity extends AppCompatActivity {
-    private Button sendBtn = null;
+    private Button sendBtn;
+    private EditText editTextArea;
+    private TextView requestTextArea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asynchronous);
 
+        editTextArea = findViewById(R.id.editText);
+        requestTextArea = findViewById(R.id.responseText);
         sendBtn = findViewById(R.id.sendButton);
+
+        requestTextArea.setMovementMethod(new ScrollingMovementMethod());
+
         sendBtn.setOnClickListener((v) -> {
             SymComManager mcm = new SymComManager();
-            Request request = new Request("http://sym.iict.ch/rest/txt", "Affaire Robel Teklehaimanot", "text/plain");
+            Request request = new Request("http://sym.iict.ch/rest/txt", editTextArea.getText().toString(), "text/plain");
             mcm.setCommunicationEventListener(
                     response -> {
-                        System.out.println(response);
+                        requestTextArea.setText(response);
                         return true;
                     }
             );
